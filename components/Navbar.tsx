395f16c4878b2008b51c648d74c7cbaf1b92f15d
Akeme <tedake36@gmail.com>
2023-10-17 16:13:19 +0300

    adding new public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,55 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { NAV_LINKS } from "@/constants";
+import Button from "./Button";
+import { useState } from "react";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  console.log(`this is ${toggle}`);
   return (
     <nav
       className=" border-2 border-red-500 flexBetween max-container   padding-container {
       relative z-30 py-5"
-    ></nav>
+    >
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((item) => (
+          <Link
+            href={item.href}
+            key={item.key}
+            className=" regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+        onClick={() => setToggle(true)}
+      />
+
+      {toggle && <p>skdffkfdk</p>}
+    </nav>
   );
 };
 
